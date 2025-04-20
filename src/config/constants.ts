33d44407569d91e353ae20a8838125639b3e13bc
Benjamin <benuserxxx@gmail.com>
2025-04-19 19:45:58 -0500

    202502191

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,6 +2,7 @@
   process.env.NODE_ENV === "development"
     ? "http://localhost/conectserv/conectserv_backend/public/api"
     : "https://conectserv.perudevsolutions.com/api";
+    "http://127.0.0.1:8000/api"
 */
 
-export const API_BASE_URL = "http://127.0.0.1:8000/api";
+export const API_BASE_URL = "https://conectserv.perudevsolutions.com/api";
